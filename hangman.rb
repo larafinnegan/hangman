c1abfeccf7bcc067549abcf904296de8188e9dd8
Lara Finnegan <lcfinnegan@gmail.com>
2015-01-30 21:38:56 -0600

    added save game methods

--- hangman.rb
+++ hangman.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 class Board
   attr_accessor :board, :word
 
@@ -28,12 +30,31 @@ class Game
   def initialize
     lines = File.readlines("words.txt")
     word = ""
-    while word.length < 6 || word.length > 13 || word == word.capitalize
+    until word.length.between?(6, 13) && word == word.downcase
       word = lines[rand(lines.length)]
     end
     @board = Board.new(word.chars.to_a)
     @guesses = 12
     @letters = []
+  end
+
+  def menu
+    puts "Would you like to start a new game or load a saved game?"
+    puts "Presss 0 to start a new game, or enter the number of one of the following saved games:"
+    Dir.exists?("saved_games") ? (puts Dir["saved_games/*"]) : (puts "No saved games!  Please press 0.")
+    choice = gets.chomp
+    count = Dir[File.join("saved_games", '**', '*')].count { |file| File.file?(file) }
+    until ("0"..count.to_s).include?(choice)
+      puts "Invalid input.  Please enter 0 for a new game, or the game number of a saved game:"
+      choice = gets.chomp
+    end
+    if ("1"..count.to_s).include?(choice)
+      File.open("saved_games/game_#{choice}.yaml") {|yf| YAML::load(yf) }
+    end
+  end
+
+  def load_file
+    File.open("saved_games/game_3.yaml") {|yf| YAML::load(yf) }
   end
 
   def guess
@@ -57,19 +78,28 @@ class Game
     end
   end
 
+  def save_game
+    Dir.mkdir("saved_games") unless Dir.exists? ("saved_games")
+    count = Dir[File.join("saved_games", '**', '*')].count { |file| File.file?(file) }
+    File.open("saved_games/game_#{count + 1}.yaml", "w") do |out|
+      YAML.dump(self, out)
+    end
+  end
+
   def play
     puts "Welcome to Hangman!\n\n"
+    menu if Dir.exists?("saved_games")
     board.display
     until @guesses == 0 || board.win?
       puts "\n\nYou have #{guesses} guesses remaining.\n\n"
       puts "You have already guessed the following letters: #{@letters.join(", ")}\n\n"
       check_guess
       board.display
+      save_game
     end
     board.win? ? (puts "\n\nCongrats, you win!") : (puts "\n\nBetter luck next time!")
   end
 end
 
 game = Game.new
-game.play
-
+game.load_file
