0252e8ff7bd27dbfdfcbdb0562164f418651bfa8
Lara Finnegan <lcfinnegan@gmail.com>
2015-01-30 18:53:38 -0600

    basic working game

--- hangman.rb
+++ hangman.rb
@@ -1,69 +1,79 @@
-class Player
-
-end
-
-
 class Board
-attr_accessor :board, :word
+  attr_accessor :board, :word
 
-def initialize(word, board=Array.new(word.length-1, "_"))
-@word = word
-@board = board
-puts word
+  def initialize(word, board=Array.new(word.length-1, "_"))
+    @word = word
+    @board = board
+    puts word
+  end
+
+  def display
+    print board.join(" ")
+  end
+
+  def correct_guess?(guess)
+    word.include?(guess)
+  end
+
+  def win?
+    board.none? {|x| x == "_"}
+  end
+
+  def update(guess)
+    word.each_with_index {|letter, index| board[index] = letter if letter == guess}
+    guess
+  end
 end
 
-def display
-print board.join(" ")
-end
-
-def win?
-board.none? {|x| x == "_"}
-end
-
-def turn(guess)
-word.each_with_index {|letter, index| board[index] = letter if letter == guess}
-end
-
-
-end
 
 class Game
-attr_accessor :board, :player, :guesses, :letters
+  attr_accessor :board, :guesses, :letters
 
-def initialize
-  lines = File.readlines("words.txt")
-  word = ""
-  while word.length < 5 || word.length > 12 || word == word.capitalize
-  word = lines[rand(lines.length)]
+  def initialize
+    lines = File.readlines("words.txt")
+    word = ""
+    while word.length < 6 || word.length > 13 || word == word.capitalize
+      word = lines[rand(lines.length)]
+    end
+    @board = Board.new(word.chars.to_a)
+    @guesses = 12
+    @letters = []
   end
-  @board = Board.new(word.chars.to_a)
-  @guesses = 0
-  @letters = []
- end
- 
 
+  def guess
+    puts "Please guess a letter:\n\n"
+    guess = gets.chomp.downcase
+    until ("a".."z").to_a.include?(guess) && !letters.include?(guess) 
+      puts "That's not a letter, please try again." unless ("a".."z").to_a.include?(guess)
+      puts "You already tried that letter, guess again!" if letters.include?(guess) 
+      guess = gets.chomp.downcase
+    end
+    @letters << guess
+    guess
+  end
 
- def guess
- puts "Please guess a letter:\n\n"
- guess = gets.chomp.downcase
- until ("a".."z").to_a.include?(guess) && !letters.include?(guess) 
- puts "That's not a letter, please try again." unless ("a".."z").to_a.include?(guess)
- puts "You already tried that letter, guess again!" if letters.include?(guess) 
- guess = gets.chomp.downcase
- end
- guess
- end
- 
- def play
- puts "Welcome to Hangman!"
- board.display
- board.turn(guess)
- board.display
- end
- 
- 
- end
- 
- game = Game.new
- game.play
+  def check_guess
+    if board.correct_guess?(board.update(guess))
+      puts "Good guess!\n\n"
+    else
+      puts "Sorry, the word doesn't contain that letter.\n\n"
+      @guesses -= 1 
+    end
+  end
+
+  def play
+    puts "Welcome to Hangman!\n\n"
+    board.display
+    until @guesses == 0 || board.win?
+      puts "\n\nYou have #{guesses} guesses remaining.\n\n"
+      puts "You have already guessed the following letters: #{@letters.join(", ")}\n\n"
+      check_guess
+      board.display
+    end
+    board.win? ? (puts "\n\nCongrats, you win!") : (puts "\n\nBetter luck next time!")
+  end
+end
+
+game = Game.new
+game.play
 
